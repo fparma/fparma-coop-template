b09e2a4e1156b3c9ca73c059a6a860f66ca428fd
Cuel <alleee@gmail.com>
2015-12-08 17:51:12 +0100

    Up'd version and restructure

--- FP_Template.VR/fparma.hpp
+++ FP_Template.VR/fparma.hpp
@@ -1,27 +1,16 @@
+// Base functions.
+class FP {
+	#include "base\funcs.hpp"
+};
 
-// Dialogs
-#include "xtra\dialogs\dialogs.hpp"
+// Anything below can be deleted if you do not want to use it.
 
-// Functions
-class CfgFunctions {
-	version = 2.2.0;
-	createShortcuts = 1;
+// Gear funcs
+class FP_Gear {
+	#include "gear\functions\funcs.hpp"
+};
 
-	// Base functions.
-	class FP {
-		#include "base\funcs.hpp"
-	};
-
-	// Anything below can be deleted if you do not want to use it.
-	// Rember to delete the dialogs (see top)
-
-	// Gear funcs
-	class FP_Gear {
-		#include "gear\functions\funcs.hpp"
-	};
-
-	// JIP and respawn manager
-	class FP_JRM {
-		#include "xtra\jrm\funcs.hpp"
-	};
+// JIP and respawn manager
+class FP_JRM {
+	#include "xtra\jrm\funcs.hpp"
 };
